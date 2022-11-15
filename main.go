ab7929a90f6cfe15962a7fb151cc75b39e91778f
elliot <to.elliot.cheung@gmail.com>
2022-11-15 23:42:29 +0900

    try to solve 404

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ type app struct {
 
 func (a *app) run() {
 	router := gin.Default()
+	// https://stackoverflow.com/questions/55347167/gin-contrib-cors-returns-404
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, ResponseType, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 	router.GET("/", a.getAllBlogs)
 	router.Run(":8080")
 }
