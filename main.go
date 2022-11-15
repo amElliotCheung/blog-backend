806ff41cbbd05bd837c3f6b760a00475acf7baff
elliot <to.elliot.cheung@gmail.com>
2022-11-15 22:17:44 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type app struct {
+}
+
+func (a *app) run() {
+	router := gin.Default()
+	router.GET("/", a.getAllBlogs)
+	router.Run("localhost:8080")
+}
+
+func main() {
+	a := &app{}
+	a.run()
+}
