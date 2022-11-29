e8be5d315118f615379760d0af140093422f9f60
elliot <to.elliot.cheung@gmail.com>
2022-11-29 22:56:03 +0900

    Feat: add app.getBlog to enable indivitual blog api
    Also upgrade the c.Json api

--- handlers.go
+++ handlers.go
@@ -2,14 +2,53 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Blog struct {
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Content     string    `json:"content"`
+	ReleaseDate time.Time `json:"release_date"`
+}
+
+var blogs = []Blog{
+	{
+		Id:          1,
+		Title:       "one",
+		Content:     "#title\n- one\n- two\n",
+		ReleaseDate: time.Now(),
+	},
+	{
+		Id:          2,
+		Title:       "two",
+		Content:     "#title\n- one\n- two\n",
+		ReleaseDate: time.Now(),
+	},
+	{
+		Id:          3,
+		Title:       "three",
+		Content:     "#title\n- one\n- two\n",
+		ReleaseDate: time.Now(),
+	},
+}
+
 func (a *app) getAllBlogs(c *gin.Context) {
 	print("connected")
 	c.JSON(http.StatusOK, gin.H{
-		"msg": "ok",
+		"data": blogs,
+	})
+}
+
+func (a *app) getBlog(c *gin.Context) {
+	pid := c.Param("id")
+	id, _ := strconv.Atoi(pid)
+	blog := blogs[id]
+	c.JSON(http.StatusOK, gin.H{
+		"data": blog,
 	})
 }
 
