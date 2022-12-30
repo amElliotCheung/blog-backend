6330aaa9ec3c1c92887b2b92795636371394a7ac
elliot <to.elliot.cheung@gmail.com>
2022-12-30 20:44:02 +0900

    Fix: add ReleaseDate field into type Comment

--- model/comment.go
+++ model/comment.go
@@ -1,9 +1,14 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Comment struct {
-	ID      primitive.ObjectID `json:"id" bson:"_id" `
-	UserID  primitive.ObjectID `json:"user_id" bson:"user_id"`
-	Content string             `json:"content" bson:"content"`
+	ID          primitive.ObjectID `json:"id" bson:"_id" `
+	UserID      primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Content     string             `json:"content" bson:"content"`
+	ReleaseDate time.Time          `json:"release_date" bson:"release_date"`
 }
